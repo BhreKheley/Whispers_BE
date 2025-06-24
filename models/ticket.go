a21a1ac4efac800b56c23e15c28cf2aed4d4aeee
BhreKheley <bhrenabil@gmail.com>
2025-06-24 14:45:26 +0700

    Update Booking Logic

--- models/ticket.go
+++ models/ticket.go
@@ -7,7 +7,7 @@ import (
 type Ticket struct {
 	ID          uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primary_key"`
 	BookingID   uuid.UUID `gorm:"type:uuid;not null"`
-	SeatID      uuid.UUID `gorm:"type:uuid;not null"`
+	SeatID      uuid.UUID `gorm:"type:uuid;not null;unique"`
 	TiketKode   string    `gorm:"type:varchar(100);not null;unique"`
 	QRPath      string    `gorm:"type:text"`  // path file QR code
 	PDFPath     string    `gorm:"type:text"`  // path file PDF e-ticket
